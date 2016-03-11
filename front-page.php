c09245979380aaddd0cceeb9f21f2473c3155daa
Alicia Ramirez <aramirez@rrc.ca>
2016-03-11 11:54:23 -0600

    Added a slider
    Added Flexslider scripts in functions.php, and then showed images in a
    slider in front-page.

--- front-page.php
+++ front-page.php
@@ -10,11 +10,47 @@ get_header(); ?>
     <div id="primary" class="content-area">
         <main id="main" class="site-main" role="main">
         
+        <!-- Slider -->
+        <?php
+        $args = array(
+            'post_type'         => 'dish',
+            'posts_per_page'    => 5,
+            'orderby'           => 'rand',    
+            'meta_key'          => 'slideshow_picture',
+            'meta_value'        => false,
+            'meta_compare'      => '!='
+        );
+        $slider = new WP_Query($args);
+            
+        if ($slider->have_posts()):
+                    ?>
+            <div class="flexslider">
+              <ul class="slides">
+              <?php
+            while($slider->have_posts()): 
+                $slider->the_post();
+        ?>
+           
+           <?php
+        //var_dump(get_field('slideshow_picture'));
+         $imageArray = get_field('slideshow_picture'); // Array returned by Advanced Custom Fields
+         $imageAlt = $imageArray['alt']; // Grab, from the array, the 'alt'
+         $imageThumbURL = $imageArray['url']; //grab from the array the image URL
+         ?>
+ 
+          <li><img src="<?php echo $imageThumbURL;?>" alt="<?php echo $imageAlt;?>"></li>
+       
+<?php endwhile;?>
+         </ul>
+        </div>
+         <?php
+          endif;?>
+        
         <div>
             <h2>Latest Blog Posts</h2>
 <?php
-$latest_blog = new WP_Query("posts_per_page=1");
             
+$latest_blog = new WP_Query("posts_per_page=1");
 if ($latest_blog->have_posts()):
     while($latest_blog->have_posts()):
         $latest_blog->the_post();
--- functions.php
+++ functions.php
@@ -119,6 +119,12 @@ function mexican_poutine_scripts() {
 	wp_enqueue_script( 'mexican-poutine-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
 	wp_enqueue_script( 'mexican-poutine-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+    
+    if(is_front_page()){
+        wp_enqueue_style('flexslider', get_template_directory_uri() . '/js/flexslider.css');
+        wp_enqueue_script('flexslider', get_template_directory_uri() . '/js/jquery.flexslider-min.js', array('jquery'), '2.6.0');
+        wp_enqueue_script('flexslider-init', get_template_directory_uri() . '/js/flexslider-init.js', array('flexslider'), '0.1');
+    }
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
